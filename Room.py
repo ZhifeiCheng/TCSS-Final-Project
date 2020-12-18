cff6a9655432cb659ee1de1b3e979ae2b3043541
deleok <deleok@uw.edu>
2020-12-17 19:08:31 -0800

    Update Room.py

--- Room.py
+++ Room.py
@@ -1,5 +1,8 @@
 class Room:
     def __init__(self, room_content=None):
+        """
+        Constructor for an empty Room that has not been visited.
+        """
         self.__point = 0
         self.__vision_potion = None
         self.__visited = False
@@ -8,6 +11,7 @@ class Room:
 
     @property
     def room_content(self):
+        """"Gives other classes access to the private field of self.__room_content."""
         return self.__room_content
 
     @room_content.setter
@@ -20,6 +24,7 @@ class Room:
 
     @property
     def room_matrix(self):
+        """"Gives other classes access to the private field of self.__room_matrix."""
         return self.__room_matrix
 
     @room_matrix.setter
@@ -31,13 +36,16 @@ class Room:
 
     @property
     def vision_potion_rooms(self):
+        """"Gives other classes access to the private field of self.__vision_potion."""
         return self.__vision_potion
 
     @vision_potion_rooms.setter
     def vision_potion_rooms(self, rooms):
+        """Assign certain rooms to contain vision potions."""
         self.__vision_potion = rooms
 
     def __str__(self):
+        """ Returns a str representation of the room. """
         res = ""
         for row in self.__room_matrix:
             res += " ".join(row) + "\n"
@@ -45,10 +53,12 @@ class Room:
 
     @property
     def is_visited(self):
+        """"Gives other classes access to the private field of self.__visited."""
         return self.__visited
 
     @is_visited.setter
     def is_visited(self, is_visited: bool):
+        """Allows other classes to modify the value of self.__visited as the game is played."""
         self.__visited = is_visited
 
     @property
